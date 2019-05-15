2bcc48180476c65de07b623a4cd8cee15476e2ce
bourd0n <andone74@gmail.com>
2019-05-15 23:53:50 +0300

    9. Palindrome Number. Second solution

--- src/main/kotlin/com/bourd0n/leetcode/palindromenumber/Solution.kt
+++ src/main/kotlin/com/bourd0n/leetcode/palindromenumber/Solution.kt
@@ -2,6 +2,19 @@ package com.bourd0n.leetcode.palindromenumber
 
 class Solution {
     fun isPalindrome(input: Int): Boolean {
+        if (input < 0 || (input % 10 == 0 && input != 0)) {
+            return false
+        }
+        var x = input
+        var rev = 0
+        while (x > rev) {
+            rev = rev * 10 + x % 10
+            x /= 10
+        }
+        return x == rev || x == rev / 10 ;
+    }
+
+    fun isPalindrome2(input: Int): Boolean {
         if (input < 0) {
             return false
         }
